1b4a208318eb653248c22d54182d377a23ae665a
hanan <hasyrawi@gmail.com>
2021-04-03 17:04:51 +0800

    tweaks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
 import Image from 'next/image'
+import { useWindowSize } from '../../hooks/window'
+import SearchBar from './Search'
 
 /**
  * Header Component
  * @returns JSX.IntrinsicElements.nav
  */
 const Header = () => {
+   const window = useWindowSize()
+   
    return (
       <header className="text-gray-400 bg-gray-900 body-font" id="header">
-         <div className="flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <Image src='/logo_3.png' width={120} height={120} />
+         <div className="flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+            {window.width <= 800 ? (<Image src='/logo_panjang.png' width={300} height={120} />) : (<Image src='/logo_3.png' width={120} height={120} />) }
             <HeaderItem />
+            <SearchBar />
          </div>
       </header>
    )
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import Header from './Header'
-
+import SearchBar from './Search'
 export {
    Header,
+   SearchBar,
 }
