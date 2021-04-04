93bade6e7273cac646604d2880ac0121c20d889b
hanan <hasyrawi@gmail.com>
2021-04-04 21:22:45 +0800

    adding disqus

--- src/pages/watch/[slug].jsx
+++ src/pages/watch/[slug].jsx
@@ -1,7 +1,7 @@
 import Layout from "../../components/layout"
 import { withRouter } from 'next/router'
 import { VideoPlayer } from "../../components/VideoPlayer";
-
+import { DiscussionEmbed } from 'disqus-react';
 
 const Watch = () => {
    return (
@@ -10,6 +10,9 @@ const Watch = () => {
             <div className="block w-full md:flex ">
                <VideoContainer />
             </div>
+         </section>
+         <section className="text-gray-400 bg-gray-800 rounded-lg body-font mb-3 p-3">
+            <DiscussionEmbed shortname='sektekomik-com-1' />
          </section>
       </Layout>
    )
